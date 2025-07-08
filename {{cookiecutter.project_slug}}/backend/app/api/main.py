61a911bb217c99c1c7951e639086ed916e7e56ab
gpradinett <gpradinett@gmail.com>
2025-07-08 04:23:06 -0500

    🔑 feat: Include Client API routes in main router.

--- {{cookiecutter.project_slug}}/backend/app/api/main.py
+++ {{cookiecutter.project_slug}}/backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, login, password_reset
+from app.api.routes import users, login, password_reset, clients
 
 
 api_router = APIRouter()
@@ -8,3 +8,4 @@ api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(login.router)
 api_router.include_router(password_reset.router, prefix="/password-reset")
+api_router.include_router(clients.router, prefix="/clients", tags=["clients"])
