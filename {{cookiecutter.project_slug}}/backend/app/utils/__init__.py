52ec889dc7a4ed860e51a7a9f14a4885600d2365
gpradinett <gpradinett@gmail.com>
2025-07-12 04:06:46 -0500

    🐛 Fix: Resolve F401 (unused imports) in app/utils/__init__.py.

--- {{cookiecutter.project_slug}}/backend/app/utils/__init__.py
+++ {{cookiecutter.project_slug}}/backend/app/utils/__init__.py
@@ -1,4 +1,4 @@
-from app.utils.email_utils import EmailData, send_email, generate_new_account_email, generate_password_reset_email, render_email_template
-from app.utils.datetime_utils import Dates
-from app.utils.text_utils import TextFormat
-from app.utils.random_utils import RandomDigits
+from app.utils.email_utils import EmailData as EmailData, send_email as send_email, generate_new_account_email as generate_new_account_email, generate_password_reset_email as generate_password_reset_email, render_email_template as render_email_template
+from app.utils.datetime_utils import Dates as Dates
+from app.utils.text_utils import TextFormat as TextFormat
+from app.utils.random_utils import RandomDigits as RandomDigits
