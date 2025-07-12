ef039635c29957855a33f074f04aba7d56816327
gpradinett <gpradinett@gmail.com>
2025-07-12 04:03:51 -0500

    🐛 Fix: Resolve F401 (unused imports) in app/crud/__init__.py.

--- {{cookiecutter.project_slug}}/backend/app/crud/__init__.py
+++ {{cookiecutter.project_slug}}/backend/app/crud/__init__.py
@@ -1,15 +1,15 @@
 from .user import (
-    create_user,
-    get_user_by_email,
-    update_user,
-    get_multiple_users,
-    get_user_by_password_reset_token,
-    delete_user,
+    create_user as create_user,
+    get_user_by_email as get_user_by_email,
+    update_user as update_user,
+    get_multiple_users as get_multiple_users,
+    get_user_by_password_reset_token as get_user_by_password_reset_token,
+    delete_user as delete_user,
 )
 from .client import (
-    create_client,
-    get_client_by_client_id,
-    update_client,
-    delete_client,
-    get_multiple_clients,
+    create_client as create_client,
+    get_client_by_client_id as get_client_by_client_id,
+    update_client as update_client,
+    delete_client as delete_client,
+    get_multiple_clients as get_multiple_clients,
 )
